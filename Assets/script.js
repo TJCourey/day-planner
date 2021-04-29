5b908c6c9334d9239461e8407b7281b3c3065e43
Thor138 <tjcourey84@gmail.com>
2021-04-28 19:57:40 -0600

    initial html styling and pseudo code

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// add day display at top of
+// add time tracker
+// link time tracker to hours on index.html
+// assign classes to lines based on past present and future
+// add text input field to center column on click
+// save contents of text input field to local storage
+//display contents of local storage to corresponding hours
+// contesnts stay visible on page reload
